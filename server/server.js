3b02b070b7950d824b877e59a5ab512119662e37
chantelle mckenzie <mrspasceri@hotmail.com>
2022-02-06 07:47:36 -0800

    added components folder in client side

--- server/server.js
+++ server/server.js
@@ -2,8 +2,15 @@ const http = require("http");
 const express = require("express");
 const app = express()
 const server = http.createServer(app)
-// const io = socketio(server)
+//const io = socketio(server)
 
 const PORT = process.env.PORT || 5000
+
+// io.on("connection", (socket) => {
+//     console.log('A Connection has been made')
+//     socket.on('disconnect', ()=> {
+//         console.log('A disconnection has been made')
+//     })
+// })
 
 server.listen(PORT, () => console.log(`Server is Quannected to Port ${PORT}`))
