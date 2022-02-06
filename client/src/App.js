bf4dfacc43e7a1c5db8c38c608b4206200fa21d1
chantelle mckenzie <mrspasceri@hotmail.com>
2022-02-06 07:50:55 -0800

    set up Routes in client folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,12 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Chat from "./components/Chat";
 
 const App = () => {
-    <h1>App Successfully rendered.</h1>
+  <Router>
+    <Route path="/" exact component={Home}/>
+    <Route path="/chat" component={Chat}/>
+  </Router>
 }
 export default App;
